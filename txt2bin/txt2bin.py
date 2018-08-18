543a318e8e1834c15e77a57a5bfbe431546f6c9e
creat2012 <zkccst@gmail.com>
2018-08-18 14:50:33 +0800

    updata the txtbin

--- txt2bin/txt2bin.py
+++ txt2bin/txt2bin.py
@@ -1,16 +1,38 @@
-
+# -*- coding:utf-8 -*-
+# this script is for transform *.txt to *.bin
+# in my use, i transfrom the double(4bit) point to half(2bit) point (bin)
+# in my txt, the double is lines to lines
+# the out.bin is the result bin file in your work dir
 import numpy as np
+import logging
+import os
+logging.basicConfig(level=logging.INFO	,
+                    format='%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s') 
 
+class Txt2bin(object):
+	def __init__(self, txt_path):
+		self.txt_path_ = txt_path
+		if not os.path.exists(self.txt_path_):
+			logging.info("*.txt file is not found :)")
 
-file_txt = open("./out.txt")
-file_data = file_txt.readlines()
-data = []
-lines = 0
-for item in file_data:
-    lines += 1
-    data.append(float(item))
-data = np.array(data)
-data.astype(np.float16).tofile("out.bin")
-for i in range(169):
-    print (i, data[i])
-print ("=========================")
+	def trans(self):
+		file_txt = open(self.txt_path_)
+		file_data = file_txt.readlines() # in your txt , double point number is line by line
+		lines = 0
+		data = []
+		for item in file_data:
+			try:
+				data.append(float(item))
+			except ValueError:
+				logging.error("your data is not all double :)")
+			lines += 1
+			if lines % 1000 == 0:
+				logging.info("readed %d double point number :)" % (lines))
+		logging.info("total readed %d double point number :)" % (lines))
+		data = np.array(data)
+		data.astype(np.float16).tofile("./out.bin") # out.bin is the bin file you transformed
+		logging.info("done for to out.bin :)")
+
+if __name__ == '__main__':
+	trans = Txt2bin("./in.txt") # in.txt is your txt file will to transform
+	trans.trans()
