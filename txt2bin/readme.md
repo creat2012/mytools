2db80a9110a0dacd9dc60cc500676e0fbf5601e2
creat2012 <zkccst@gmail.com>
2018-08-21 13:57:34 +0800

    add command line argv

---
+++ txt2bin/readme.md
@@ -0,0 +1,21 @@
+#### use for
+change in_yolo2.txt(double point number, 4bit) to in_yolo2.bin(float point number, 2bit)
+
+
+
+#### data/
+data/
+├── in_yolo2.bin
+├── in_yolo2.txt
+├── in_yolo3.bin
+├── in_yolo3.txt
+├── out_yolo2.bin
+├── out_yolo2.txt
+├── out_yolo3.bin
+├── out_yolo3.txt
+├── yolo2_shape
+└── yolo3_shape
+
+
+
+
--- txt2bin/txt2bin.py
+++ txt2bin/txt2bin.py
@@ -6,33 +6,38 @@
 import numpy as np
 import logging
 import os
+import sys
 logging.basicConfig(level=logging.INFO	,
                     format='%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s') 
 
 class Txt2bin(object):
-	def __init__(self, txt_path):
-		self.txt_path_ = txt_path
-		if not os.path.exists(self.txt_path_):
-			logging.info("*.txt file is not found :)")
+    def __init__(self, txt_path):
+        self.txt_path_ = txt_path
+        if not os.path.exists(self.txt_path_):
+            logging.error("*.txt file is not found :)")
+        self.txt_name_ = txt_path.split(".")[0]
 
-	def trans(self):
-		file_txt = open(self.txt_path_)
-		file_data = file_txt.readlines() # in your txt , double point number is line by line
-		lines = 0
-		data = []
-		for item in file_data:
-			try:
-				data.append(float(item))
-			except ValueError:
-				logging.error("your data is not all double :)")
-			lines += 1
-			if lines % 1000 == 0:
-				logging.info("readed %d double point number :)" % (lines))
-		logging.info("total readed %d double point number :)" % (lines))
-		data = np.array(data)
-		data.astype(np.float16).tofile("./out.bin") # out.bin is the bin file you transformed
-		logging.info("done for to out.bin :)")
+    def trans(self):
+        file_txt = open(self.txt_path_)
+        file_data = file_txt.readlines() # in your txt , double point number is line by line
+	lines = 0
+	data = []
+	for item in file_data:
+            try:
+                data.append(float(item))
+            except ValueError:
+                logging.error("your data is not all double :)")
+            lines += 1
+            if lines % 1000 == 0:
+                logging.info("readed %d double point number :)" % (lines))
+	logging.info("total readed %d double point number :)" % (lines))
+	data = np.array(data)
+	data.astype(np.float16).tofile(self.txt_name_ + ".bin") # out.bin is the bin file you transformed
+	logging.info("done for to out.bin :)")
 
 if __name__ == '__main__':
-	trans = Txt2bin("./in.txt") # in.txt is your txt file will to transform
-	trans.trans()
+    if len(sys.argv) < 2:
+        logging.error("please given the file(txt) your will transform")
+    else:
+        transer = Txt2bin(sys.argv[1]) # the second argv is file will transfrom
+        transer.trans()
